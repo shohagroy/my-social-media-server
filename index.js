13e02119781b50be8e65349a67b7f7814c6da717
shohagroy <pkshohag240@gmail.com>
2022-12-29 03:56:07 +0600

    weShare server added and like function added

---
+++ index.js
@@ -0,0 +1,161 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
+
+const port = process.env.POST || 5000;
+const app = express();
+
+// jwt secret
+const secret = process.env.REACT_APP_JWT_SECRET_ACCESS_TOKEN;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// all collection
+
+// middleware function
+const jwtVerify = (req, res, next) => {
+  const bearerToken = req.headers.authorization;
+
+  if (!bearerToken) {
+    return res.status(401).send({ massege: "unauthorized access" });
+  }
+  const jwtToken = bearerToken.split(" ")[1];
+
+  jwt.verify(jwtToken, secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ massege: "unauthorized access" });
+    } else {
+      req.decoded = decoded;
+      next();
+    }
+  });
+};
+
+// database
+// const uri = process.env.DATABASE_URI;
+const uri = "mongodb://localhost:27017";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+    const usersCollection = client.db("WeShare").collection("users");
+    const postsCollection = client.db("WeShare").collection("allPosts");
+
+    const userVerify = async (req, res, next) => {
+      const verifyEmail = req.decoded.email;
+      const query = { email: verifyEmail };
+      const result = await usersCollection.findOne(query);
+
+      if (result) {
+        next();
+      } else {
+        return res.status(401).send({ massege: "unauthorized access" });
+      }
+    };
+
+    const makeProfilePicture = async (req, res, next) => {
+      const profilePicture = req.body.isProfilePicture;
+      const newProfilePicture = req.body.image;
+      const userEmail = req.decoded.email;
+
+      if (profilePicture) {
+        const query = { email: userEmail };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            photoUrl: newProfilePicture,
+          },
+        };
+        const result = await usersCollection.updateOne(
+          query,
+          updateDoc,
+          options
+        );
+      }
+      next();
+    };
+
+    // creact user
+    app.post("/createUser", async (req, res) => {
+      const userInfo = req.body;
+      const userEmail = { email: userInfo.email };
+      const jwtToken = jwt.sign(userEmail, secret, { expiresIn: "3h" });
+      const result = await usersCollection.insertOne(userInfo);
+      res.send({ jwtToken });
+    });
+
+    app.get("/jwtCrateLoginUser", async (req, res) => {
+      const userEmail = { email: req.query.email };
+      const jwtToken = jwt.sign(userEmail, secret, { expiresIn: "3h" });
+      res.send({ jwtToken });
+    });
+
+    app.post(
+      "/createNewPost",
+      jwtVerify,
+      userVerify,
+      makeProfilePicture,
+      async (req, res) => {
+        const newPostData = req.body;
+        const result = await postsCollection.insertOne(newPostData);
+        res.send(result);
+      }
+    );
+
+    app.get("/allFeedsData", jwtVerify, userVerify, async (req, res) => {
+      const query = {};
+      const result = await postsCollection
+        .find(query)
+        .sort({ date: 1 })
+        .toArray();
+      res.send(result);
+    });
+    app.get("/findUser", jwtVerify, userVerify, async (req, res) => {
+      const userEmail = req.query.userEmail;
+      const query = { email: userEmail };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/setNewReact", jwtVerify, userVerify, async (req, res) => {
+      const id = req.query.id;
+      const updatedReact = req.body;
+
+      const query = { _id: ObjectId(id) };
+
+      const post = await postsCollection.findOne(query);
+      const currentReact = post.totalReact + 1;
+
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          totalReact: currentReact,
+          react: updatedReact,
+        },
+      };
+      const result = await postsCollection.updateOne(query, updateDoc, options);
+      console.log(result);
+      res.send(result);
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("WeShare Media - Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`WeShare Media - Server is Running port: ${port}`);
+});
